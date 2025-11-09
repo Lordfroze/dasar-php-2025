71a4cc544ee6db1513641d6bebc058da308d5661
yoga <yogaadipratama22@gmail.com>
2025-11-09 19:44:02 +0700

    variabel

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,9 @@
 <?php
 echo "Hello World <h1>Kelas Full Stack</h1>";
+// komentar
+/*
+komentar 
+banyak 
+baris
+*/
 ?>
---
+++ variabel.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "yoga";
+$umur = 27;
+$lima = 5;
+
+echo "namaku $nama, umurku $umur tahun";
+echo "<br>";
+echo "lima tahun lagi umurku ", $umur + $lima, "tahun";
+?>
