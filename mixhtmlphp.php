bef2594f39dafc128ef037f1bf449ffa08b0ad05
lordfroze <yogaadipratama22@gmail.com>
2025-11-10 14:07:40 +0700

    mixhtmlphp

---
+++ mixhtmlphp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mix HTML PHP</title>
+</head>
+<body>
+    <?php 
+        $nama = "Yoga";
+    ?>
+    <h1>Hello World</h1>
+    <h2 style="color: blue">Namaku <?php echo $nama; ?></h2>
+</body>
+</html>
