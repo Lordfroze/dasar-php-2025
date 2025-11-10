c1c892ddcd09b3360ff9bd93fcb7ad67c860a151
lordfroze <yogaadipratama22@gmail.com>
2025-11-10 16:58:49 +0700

    array asosoatif

---
+++ array.php
@@ -0,0 +1,10 @@
+<?php 
+    $buah = ["Apel", "Jeruk", "Mangga", 1 , 3];
+
+    echo $buah[0]; // Apel
+    echo $buah[1]; // Jeruk
+    echo $buah[2]; // Mangga
+    echo "<br>";
+    echo $buah[3]; // 1
+    echo $buah[4]; // 3
+?>
---
+++ arrayasosiatif.php
@@ -0,0 +1,11 @@
+<?php 
+ $mahasiswa = [
+    "nama" => "Budi",
+    "umur" => 20,
+    "alamat" => "Jl. Merdeka No. 123"
+ ];
+
+    echo $mahasiswa["nama"]; // Budi
+    echo $mahasiswa["umur"]; // 20
+    echo $mahasiswa["alamat"]; // Jl. Merdeka No. 123
+?>
